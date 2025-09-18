ec4309e5bff7776789628d6b7aeafcb52310ea15
AatkaMeraj <aatkameraj1425@gmail.com>
2025-09-18 11:03:50 +0530

    demo.py added

--- templates/demo.py
+++ templates/demo.py
@@ -1,1 +1,9 @@
+import***REMOVED***os
+import***REMOVED***sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),***REMOVED***"..")))
+
 from***REMOVED***src.flight_price_prediction***REMOVED***import***REMOVED***logger
+
+if***REMOVED***__name__***REMOVED***==***REMOVED***"__main__":
+***REMOVED******REMOVED******REMOVED******REMOVED***logger.info("✅***REMOVED***Demo***REMOVED***script***REMOVED***is***REMOVED***running***REMOVED***successfully!")
