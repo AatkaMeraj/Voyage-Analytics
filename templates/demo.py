f3082883fb20d716b0c4fc653b62851b445e84c8
AatkaMeraj <aatkameraj1425@gmail.com>
2025-09-18 11:03:50 +0530

    demo.py added

--- templates/demo.py
+++ templates/demo.py
@@ -1,1 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from src.flight_price_prediction import logger
+
+if __name__ == "__main__":
+    logger.info("✅ Demo script is running successfully!")
